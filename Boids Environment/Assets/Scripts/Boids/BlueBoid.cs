acbb23479f0f02f6e2bed7fb7c702161f59364f7
PairOfPants <adenham112@gmail.com>
2023-05-15 10:57:52 -0400

    Did a thing

--- Boids Environment/Assets/Scripts/Boids/BlueBoid.cs
+++ Boids Environment/Assets/Scripts/Boids/BlueBoid.cs
@@ -15,7 +15,7 @@ public class BlueBoid : Boid {
     public void Initialize() //(BlueSettings settings, Transform target) {
     {
         base.Initialize(cachedTransform);
-        print("hi");
+        //print("hi");
     }
     
    /*  public void UpdateBlueBoid () 
